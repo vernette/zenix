41ed35125817b0bc78b0611bdcf0d3cd4f2f89ad
Nikita Skryabin <nekit2271@gmail.com>
2025-02-19 20:20:31 +0300

    fix(flake.nix): enable allowUnfree for unstable channel to include non-free software packages

--- nix/flake.nix
+++ nix/flake.nix
@@ -15,7 +15,10 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      unstable = nixpkgs-unstable.legacyPackages.${system};
+      unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in
     {
       nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
