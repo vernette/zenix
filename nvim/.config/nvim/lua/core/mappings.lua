2ce841b247108fd0bdacf889e93a09859222831e
Nikita Skryabin <nekit2271@gmail.com>
2025-03-11 13:51:22 +0300

    feat(mappings.lua): add code action mapping for quick fixes

--- nvim/.config/nvim/lua/core/mappings.lua
+++ nvim/.config/nvim/lua/core/mappings.lua
@@ -43,6 +43,7 @@ map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "List Diagnostics" })
 map("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous Diagnostic" })
 map("n", "]d", vim.diagnostic.goto_next, { desc = "Next Diagnostic" })
 map("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show Diagnostics" })
+map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
 
 -- Outline
 map("n", "<leader>ps", ":Outline<CR>", { desc = "Toggle Outline" })
