c8466c3b272b6b3e1af7f8ed213efd017a41e248
Nikita Skryabin <nekit2271@gmail.com>
2025-03-11 11:58:06 +0300

    feat(cmp): update completion formatting to use lspkind for better visuals

--- nvim/.config/nvim/lua/configs/cmp.lua
+++ nvim/.config/nvim/lua/configs/cmp.lua
@@ -1,27 +1,34 @@
 return {
   formatting = {
     format = function(entry, vim_item)
-      -- Use a simple format without lspkind for now, since we're skipping LSP
-      vim_item.menu = ({
-        buffer = "[Buffer]",
-        path = "[Path]",
-        luasnip = "[Snippet]",
-        cmdline = "[Cmdline]",
-        dotenv = "[Env]",
-        codeium = "[Codeium]",
-      })[entry.source.name]
-      return vim_item
+      local lspkind = require("lspkind")
+      return lspkind.cmp_format({
+        mode = "symbol_text",
+        menu = ({
+          buffer = "[Buffer]",
+          path = "[Path]",
+          luasnip = "[Snippet]",
+          cmdline = "[Cmdline]",
+          dotenv = "[Env]",
+          codeium = "[Codeium]",
+        }),
+        maxwidth = 50,
+        ellipsis_char = "...",
+      })(entry, vim_item)
     end,
   },
+
   snippet = {
     expand = function(args)
       require("luasnip").lsp_expand(args.body)
     end,
   },
+
   window = {
     completion = require("cmp").config.window.bordered(),
     documentation = require("cmp").config.window.bordered(),
   },
+  
   mapping = require("cmp").mapping.preset.insert({
     ["<C-b>"] = require("cmp").mapping.scroll_docs(-4),
     ["<C-f>"] = require("cmp").mapping.scroll_docs(4),
@@ -29,6 +36,7 @@ return {
     ["<C-e>"] = require("cmp").mapping.abort(),
     ["<CR>"] = require("cmp").mapping.confirm({ select = true }),
   }),
+  
   sources = require("cmp").config.sources({
     {
       name = "luasnip",
@@ -55,6 +63,7 @@ return {
       priority = 350,
     },
   }),
+  
   cmdline = {
     search = {
       mapping = require("cmp").mapping.preset.cmdline(),
