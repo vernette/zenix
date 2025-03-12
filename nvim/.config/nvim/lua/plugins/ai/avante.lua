acdbe24387e3d03b86e5b4e41e95702b2bb4e122
Nikita Skryabin <nekit2271@gmail.com>
2025-03-12 14:01:04 +0300

    chore(avante.lua): disable plugin

--- nvim/.config/nvim/lua/plugins/ai/avante.lua
+++ nvim/.config/nvim/lua/plugins/ai/avante.lua
@@ -9,6 +9,7 @@ return {
     "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
   },
+  enabled = false,
   event = "VeryLazy",
   version = false,
   build = "make",
