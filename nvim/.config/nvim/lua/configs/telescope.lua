01191eceacc524d23c073a5b85c6a84dbcc0ea9a
Nikita Skryabin <nekit2271@gmail.com>
2025-03-12 17:49:15 +0300

    feat(telescope.lua): update layout config

--- nvim/.config/nvim/lua/configs/telescope.lua
+++ nvim/.config/nvim/lua/configs/telescope.lua
@@ -1,20 +1,42 @@
+local default_preview_cutoff = 0
+local default_preview_value = 0.5
+local custom_preview_value = 0.8
+
+local custom_layout_config = {
+  horizontal = {
+    preview_cutoff = default_preview_cutoff,
+    preview_width = custom_preview_value,
+  },
+}
+
 return {
-  -- TODO: Customize pickers
   pickers = {
-    find_files = { hidden = true },
+    find_files = { layout_config = custom_layout_config },
+
+    oldfiles = { layout_config = custom_layout_config },
+
     colorscheme = {
       enable_preview = true,
       ignore_builtins = true,
+      layout_config = custom_layout_config,
+    },
+
+    current_buffer_fuzzy_find = { sorting_strategy = "ascending" },
+
+    buffers = {
+      theme = "dropdown",
+      previewer = false,
     },
   },
 
   extensions = {
-    ["ui-select"] = { require("telescope.themes").get_dropdown({}) },
+    ["ui-select"] = { require("telescope.themes").get_cursor({}) },
     import = { insert_at_top = true },
     fzf = {},
   },
 
   defaults = {
+    layout_strategy = "horizontal",
     prompt_prefix = " ",
     selection_caret = "󱞩 ",
     path_display = { "smart" },
@@ -22,17 +44,18 @@ return {
     file_ignore_patterns = {
       "venv",
       ".venv",
+      "__pycache__",
       "node_modules",
     },
 
     layout_config = {
       vertical = {
-        preview_cutoff = 0,
-        preview_height = 0.5,
+        preview_cutoff = default_preview_cutoff,
+        preview_height = default_preview_value,
       },
       horizontal = {
-        preview_cutoff = 0,
-        preview_width = 0.6,
+        preview_cutoff = default_preview_cutoff,
+        preview_width = default_preview_value,
       },
     },
   },
