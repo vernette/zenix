62b05178f85603bcbe169a5033d7f62cbfe7fdcc
Nikita Skryabin <nekit2271@gmail.com>
2025-03-12 10:16:09 +0300

    refactor(nvim): move lazy configuration to a separate file

---
+++ nvim/.config/nvim/lua/configs/lazy.lua
@@ -0,0 +1,66 @@
+return {
+  defaults = { lazy = true },
+
+  spec = {
+    { import = "plugins.colorschemes" },
+    { import = "plugins.ui" },
+    { import = "plugins.misc" },
+    { import = "plugins.lsp" },
+    { import = "plugins.ai" },
+    { import = "plugins.markdown" },
+    { import = "plugins.completions" },
+  },
+
+  install = { colorscheme = { "tokyonight" } },
+
+  ui = {
+    border = "rounded",
+    icons = {
+      ft = "",
+      lazy = "󰂠 ",
+      loaded = "",
+      not_loaded = "",
+    },
+  },
+
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "2html_plugin",
+        "tohtml",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "matchit",
+        "tar",
+        "tarPlugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+        "tutor",
+        "rplugin",
+        "syntax",
+        "synmenu",
+        "optwin",
+        "compiler",
+        "bugreport",
+        "ftplugin",
+      },
+    },
+  },
+
+  change_detection = { enabled = false },
+
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+}
