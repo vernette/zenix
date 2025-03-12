f2d41311e9ffd7c004487e1e8d9f07338ba4e033
Nikita Skryabin <nekit2271@gmail.com>
2025-03-12 15:36:07 +0300

    feat(mappings.lua): add <Esc> keybinding to clear search highlights

--- nvim/.config/nvim/lua/core/mappings.lua
+++ nvim/.config/nvim/lua/core/mappings.lua
@@ -17,6 +17,8 @@ map("n", "<C-j>", "<C-w>j", { desc = "Move down" })
 map("n", "<C-k>", "<C-w>k", { desc = "Move up" })
 map("n", "<C-l>", "<C-w>l", { desc = "Move right" })
 
+map("n", "<Esc>", ":noh<CR>", { desc = "Clear Highlight" })
+
 -- NvimTree
 map("n", "<C-n>", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
 map("n", "<leader>e", ":NvimTreeFocus<CR>", { desc = "Focus NvimTree" })
