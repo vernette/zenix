eebf9dc13b28f1c7fa203f198430cb4584bf6f08
Nikita Skryabin <nekit2271@gmail.com>
2025-03-13 17:30:52 +0300

    fix(nvim/mini-indentscope): prevent indentscope from loading for certain filetypes

--- nvim/.config/nvim/lua/plugins/ui/mini-indentscope.lua
+++ nvim/.config/nvim/lua/plugins/ui/mini-indentscope.lua
@@ -2,5 +2,20 @@ return {
   "echasnovski/mini.indentscope",
   version = false,
   event = "BufReadPost",
-  opts = {},
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      desc = "Disable indentscope for certain filetypes",
+      pattern = {
+        "help",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "NvimTree",
+      },
+      callback = function()
+        vim.b.miniindentscope_disable = true
+      end,
+    })
+  end,
 }
