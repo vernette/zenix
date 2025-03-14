f2bf18f04aaed5fec10e9925a6e1326e61758cba
Nikita Skryabin <nekit2271@gmail.com>
2025-03-14 22:17:36 +0300

    fix(nvim/telescope): simplify layout config and remove redundant settings

--- nvim/.config/nvim/lua/configs/telescope.lua
+++ nvim/.config/nvim/lua/configs/telescope.lua
@@ -1,24 +1,10 @@
 local default_preview_cutoff = 0
-local default_preview_value = 0.5
-local custom_preview_value = 0.8
-
-local custom_layout_config = {
-  horizontal = {
-    preview_cutoff = default_preview_cutoff,
-    preview_width = custom_preview_value,
-  },
-}
 
 return {
   pickers = {
-    find_files = { layout_config = custom_layout_config },
-
-    oldfiles = { layout_config = custom_layout_config },
-
     colorscheme = {
       enable_preview = true,
       ignore_builtins = true,
-      layout_config = custom_layout_config,
     },
 
     current_buffer_fuzzy_find = { sorting_strategy = "ascending" },
@@ -49,14 +35,8 @@ return {
     },
 
     layout_config = {
-      vertical = {
-        preview_cutoff = default_preview_cutoff,
-        preview_height = default_preview_value,
-      },
-      horizontal = {
-        preview_cutoff = default_preview_cutoff,
-        preview_width = default_preview_value,
-      },
+      vertical = { preview_cutoff = default_preview_cutoff },
+      horizontal = { preview_cutoff = default_preview_cutoff },
     },
   },
 }
