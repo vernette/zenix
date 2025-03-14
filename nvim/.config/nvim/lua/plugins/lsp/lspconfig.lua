a05aee26f18c526c598f6a2c1755bfe4386cdfdd
Nikita Skryabin <nekit2271@gmail.com>
2025-03-14 22:44:35 +0300

    feat(nvim/lsp): add custom diagnostic signs

--- nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -8,6 +8,18 @@ return {
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
+    local signs = {
+      Error = " ",
+      Warn = " ",
+      Hint = "󰠠 ",
+      Info = " ",
+    }
+
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
+
     for server_name, server_settings in pairs(lsp_settings.servers) do
       local final_settings = vim.tbl_deep_extend("force", {
         capabilities = capabilities,
