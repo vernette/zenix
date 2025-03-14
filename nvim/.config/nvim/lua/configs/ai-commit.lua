6bca5ed674434fd10da77818580aac011df6767b
Nikita Skryabin <nekit2271@gmail.com>
2025-03-14 21:54:36 +0300

    feat(nvim/ai-commit): configure and integrate ai-commit plugin

---
+++ nvim/.config/nvim/lua/configs/ai-commit.lua
@@ -0,0 +1,5 @@
+return {
+  openrouter_api_key = vim.env.OPENROUTER_API_KEY, -- or set your API key directly
+  model = "qwen/qwen-2.5-72b-instruct:free", -- default model
+  auto_push = false, -- whether to automatically push after commit
+}
---
+++ nvim/.config/nvim/lua/plugins/ai/ai-commit.lua
@@ -0,0 +1,5 @@
+return {
+  "vernette/ai-commit.nvim",
+  cmd = "AICommit",
+  opts = require("configs.ai-commit"),
+}
