98b62b70d9055f536dd447ec5cf8b2deeeba22e7
Nikita Skryabin <nekit2271@gmail.com>
2025-03-15 11:11:01 +0300

    feat(nvim/nvim-tree): enhance configuration with NvChad settings

--- nvim/.config/nvim/lua/configs/nvim-tree.lua
+++ nvim/.config/nvim/lua/configs/nvim-tree.lua
@@ -1,6 +1,18 @@
 return {
+  -- Mostly from https://github.com/NvChad/NvChad/blob/v2.5/lua/nvchad/configs/nvimtree.lua
+  filters = { dotfiles = false },
+  disable_netrw = true,
+  hijack_cursor = true,
+  sync_root_with_cwd = true,
+
+  update_focused_file = {
+    enable = true,
+    update_root = false,
+  },
+
   view = {
     side = "left",
+    preserve_window_proportions = true,
     width = {
       max = 50,
       padding = 10,
@@ -8,19 +20,30 @@ return {
   },
 
   renderer = {
+    root_folder_label = false,
     highlight_git = "name",
     highlight_opened_files = "name",
-    root_folder_label = ":t",
 
     indent_markers = {
       enable = true,
       inline_arrows = false,
     },
 
-    icons = { git_placement = "after" },
+    icons = {
+      git_placement = "after",
+      glyphs = {
+        default = "󰈚",
+        folder = {
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+        },
+        git = { unmerged = "" },
+      },
+    },
   },
 
   actions = { open_file = { resize_window = false } },
-
-  filters = { dotfiles = false },
 }
