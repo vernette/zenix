6654c20472be1c20630a195d06b1c9949d1ccfdc
Nikita Skryabin <nekit2271@gmail.com>
2025-03-17 11:12:48 +0300

    feat(nvim): add codesnap configuration for code screenshots

---
+++ nvim/.config/nvim/lua/configs/codesnap.lua
@@ -0,0 +1,11 @@
+return {
+  save_path = "~/Pictures/Screenshots/Code",
+  border = "rounded",
+  has_breadcrumbs = false,
+  has_line_number = true,
+  bg_theme = "grape",
+  bg_x_padding = 122,
+  bg_y_padding = 52,
+  bg_padding = nil,
+  watermark = "",
+}
