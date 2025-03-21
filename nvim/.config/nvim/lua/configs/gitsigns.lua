be69df9f611d17f0b30f4783e1563d371ab8d5ae
Nikita Skryabin <nekit2271@gmail.com>
2025-03-21 15:12:30 +0300

    feat(nvim/gitsigns): add custom signs configuration

--- nvim/.config/nvim/lua/configs/gitsigns.lua
+++ nvim/.config/nvim/lua/configs/gitsigns.lua
@@ -1,3 +1,13 @@
+local signs = {
+  add = { text = "+" },
+  change = { text = "~" },
+  delete = { text = "_" },
+  topdelete = { text = "‾" },
+  changedelete = { text = "~" },
+}
+
 return {
+  signs = signs,
+  signs_staged = signs,
   current_line_blame_formatter = "[<author_time:%R>] <author>: <summary>",
 }
