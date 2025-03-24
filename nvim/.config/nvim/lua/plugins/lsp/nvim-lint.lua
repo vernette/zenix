1da5a53549005d4c233760fb65afd54335221af4
Nikita Skryabin <nekit2271@gmail.com>
2025-03-24 17:04:32 +0300

    refactor(nvim/lint): modify lint autocmd events

--- nvim/.config/nvim/lua/plugins/lsp/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/lsp/nvim-lint.lua
@@ -9,10 +9,10 @@ return {
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-    vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged", "TextChangedI" }, {
       group = lint_augroup,
       callback = function()
-        require("lint").try_lint()
+        lint.try_lint()
       end,
     })
   end,
