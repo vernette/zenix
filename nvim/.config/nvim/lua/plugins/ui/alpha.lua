8bd61daf9c642df53d1c67b678cb21237c31acb2
Nikita Skryabin <nekit2271@gmail.com>
2025-03-25 11:33:29 +0300

    feat(nvim/ui): add alpha-nvim dashboard

---
+++ nvim/.config/nvim/lua/plugins/ui/alpha.lua
@@ -0,0 +1,59 @@
+return {
+  "goolord/alpha-nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  event = "VimEnter",
+  opts = function()
+    local dashboard = require("alpha.themes.dashboard")
+    -- TODO: Add logo and move config to configs folder
+    -- local logo = [[
+    -- ]]
+    -- dashboard.section.header.val = vim.split(logo, "\n")
+    dashboard.section.buttons.val = {
+      dashboard.button("SPC ff", " " .. " Find Files", ":Telescope find_files<CR>"),
+      dashboard.button("SPC fo", " " .. " Recent Files", ":Telescope oldfiles<CR>"),
+      dashboard.button("SPC fw", "󰈬 " .. " Find Text", ":Telescope live_grep<CR>"),
+      dashboard.button("e", " " .. " New File", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("q", " " .. " Quit Neovim", ":qa<CR>"),
+    }
+    for _, button in ipairs(dashboard.section.buttons.val) do
+      button.opts.hl = "AlphaButtons"
+      button.opts.hl_shortcut = "AlphaShortcut"
+    end
+    dashboard.section.header.opts.hl = "AlphaHeader"
+    dashboard.section.buttons.opts.hl = "AlphaButtons"
+    dashboard.section.footer.opts.hl = "AlphaFooter"
+    dashboard.opts.layout[1].val = 8
+    return dashboard
+  end,
+  config = function(_, dashboard)
+    -- close Lazy and re-open when the dashboard is ready
+    if vim.o.filetype == "lazy" then
+      vim.cmd.close()
+      vim.api.nvim_create_autocmd("User", {
+        once = true,
+        pattern = "AlphaReady",
+        callback = function()
+          require("lazy").show()
+        end,
+      })
+    end
+
+    require("alpha").setup(dashboard.opts)
+
+    vim.api.nvim_create_autocmd("User", {
+      once = true,
+      callback = function()
+        local stats = require("lazy").stats()
+        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+        dashboard.section.footer.val = "⚡ Neovim loaded "
+          .. stats.loaded
+          .. "/"
+          .. stats.count
+          .. " plugins in "
+          .. ms
+          .. "ms"
+        pcall(vim.cmd.AlphaRedraw)
+      end,
+    })
+  end,
+}
