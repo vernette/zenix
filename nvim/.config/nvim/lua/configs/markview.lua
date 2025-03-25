74f1455aaaa4b542aff3810643ea33aa55707d75
Nikita Skryabin <nekit2271@gmail.com>
2025-03-25 14:54:43 +0300

    fix(nvim/markview): disable preview by default

--- nvim/.config/nvim/lua/configs/markview.lua
+++ nvim/.config/nvim/lua/configs/markview.lua
@@ -1,6 +1,7 @@
 return {
   max_length = 99999,
   preview = {
+    enable = false, -- Disable by default
     filetypes = { "markdown", "Avante" },
     ignore_buftypes = { "nofile" },
     icon_provider = "devicons",
