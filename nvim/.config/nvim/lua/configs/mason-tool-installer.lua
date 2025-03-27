c8ce0c43bf07cbad9c0ecab26ba66b053f25cac0
Nikita Skryabin <nekit2271@gmail.com>
2025-03-27 16:41:04 +0300

    chore(nvim/mason): adjust tool installer configuration

--- nvim/.config/nvim/lua/configs/mason-tool-installer.lua
+++ nvim/.config/nvim/lua/configs/mason-tool-installer.lua
@@ -2,18 +2,19 @@ return {
   ensure_installed = {
     -- LSP
     "lua_ls", -- Lua
-    "basedpyright", -- Python
+    "jedi-language-server", -- Python
     "bashls", -- Bash
     "dockerls", -- Dockerfile
-    "nil_ls", -- Nix
+    -- "nil_ls", -- Nix
     "json-lsp", -- JSON
     "yaml-language-server", -- YAML
+    "harper-ls", -- Grammar checker
 
     -- Formatters
     "stylua", -- Lua
     "ruff", -- Python
     "shfmt", -- Shell
-    "alejandra", -- Nix
+    -- "alejandra", -- Nix
     "prettierd", -- CSS, HTML, JSON, JavaScript, Markdown, YAML, etc
 
     -- Linters
@@ -26,6 +27,5 @@ return {
     -- statix for Nix, but it's not in Mason. Installed system-wide
     "sqlfluff", -- SQL
     "htmlhint", -- HTML
-    "codespell", -- Misspelled english words
   },
 }
