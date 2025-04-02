e148fbe6a77c279b5f7813479950631fcdc42dfa
Nikita Skryabin <nekit2271@gmail.com>
2025-04-02 11:43:14 +0300

    fix(nvim/noice): implement skip for specific notification messages

--- nvim/.config/nvim/lua/configs/noice.lua
+++ nvim/.config/nvim/lua/configs/noice.lua
@@ -1,1 +1,11 @@
-return { presets = { inc_rename = true } }
+return {
+  routes = {
+    {
+      filter = {
+        event = "notify",
+        find = "No information available",
+      },
+      opts = { skip = true },
+    },
+  },
+}
