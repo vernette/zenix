9301f0776d882aeac427512afde936304772f5e1
Nikita Skryabin <nekit2271@gmail.com>
2025-04-02 10:56:08 +0300

    refactor(nvim): update keymap function to handle string and table descriptions

--- nvim/.config/nvim/lua/core/mappings.lua
+++ nvim/.config/nvim/lua/core/mappings.lua
@@ -1,6 +1,14 @@
-local function map(mode, lhs, rhs, opts)
-  opts = opts or {}
+local function map(mode, lhs, rhs, desc_or_opts)
+  local opts = {}
+
+  if type(desc_or_opts) == "string" then
+    opts = { desc = desc_or_opts }
+  elseif type(desc_or_opts) == "table" then
+    opts = desc_or_opts
+  end
+
   opts.silent = opts.silent ~= false
+
   vim.keymap.set(mode, lhs, rhs, opts)
 end
 
@@ -8,97 +16,97 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Buffers navigation
-map("n", "]b", ":bnext<CR>", { desc = "Previous Buffer" })
-map("n", "[b", ":bprevious<CR>", { desc = "Next Buffer" })
+map("n", "]b", ":bnext<CR>", "Next Buffer")
+map("n", "[b", ":bprevious<CR>", "Previous Buffer")
 
 -- Better window navigation
-map("n", "<C-h>", "<C-w>h", { desc = "Move left" })
-map("n", "<C-j>", "<C-w>j", { desc = "Move down" })
-map("n", "<C-k>", "<C-w>k", { desc = "Move up" })
-map("n", "<C-l>", "<C-w>l", { desc = "Move right" })
+map("n", "<C-h>", "<C-w>h", "Move left")
+map("n", "<C-j>", "<C-w>j", "Move down")
+map("n", "<C-k>", "<C-w>k", "Move up")
+map("n", "<C-l>", "<C-w>l", "Move right")
 
-map("n", "<Esc>", ":noh<CR>", { desc = "Clear Highlight" })
+map("n", "<Esc>", ":noh<CR>", "Clear Highlight")
 
 -- NvimTree
-map("n", "<C-n>", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
-map("n", "<leader>e", ":NvimTreeFocus<CR>", { desc = "Focus NvimTree" })
+map("n", "<C-n>", ":NvimTreeToggle<CR>", "Toggle NvimTree")
+map("n", "<leader>e", ":NvimTreeFocus<CR>", "Focus NvimTree")
 
 -- Oil
-map("n", "-", ":Oil --float<CR>", { desc = "Open Current Directory in Oil" })
+map("n", "-", ":Oil --float<CR>", "Open Current Directory in Oil")
 
 -- ToggleTerm
-map("n", "<C-\\>", ":ToggleTerm<CR>", { desc = "Toggle Terminal" })
-map("t", "<esc>", [[<C-\><C-n>]], { desc = "Exit Terminal Mode" })
+map("n", "<C-\\>", ":ToggleTerm<CR>", "Toggle Terminal")
+map("t", "<esc>", [[<C-\><C-n>]], "Exit Terminal Mode")
 
 -- Flash
 map({ "n", "x", "o" }, "<leader>s", function()
   require("flash").jump()
-end, { desc = "Flash" })
+end, "Flash")
 
 -- BufDelete
 map("n", "<A-w>", function()
   require("bufdelete").bufdelete(0, true)
-end, { desc = "Delete Current Buffer" })
+end, "Delete Current Buffer")
 
 -- Telescope
-map("n", "<leader>th", ":Telescope colorscheme<CR>", { desc = "Choose Colorscheme" })
-map("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find Files" })
-map("n", "<leader>fw", ":Telescope live_grep<CR>", { desc = "Live Grep" })
-map("n", "<leader>fo", ":Telescope oldfiles<CR>", { desc = "Show Recent Files" })
-map("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Show Buffers" })
-map("n", "<leader>fk", ":Telescope keymaps<CR>", { desc = "Show Keymaps" })
-map("n", "<leader>fcb", ":Telescope current_buffer_fuzzy_find<CR>", { desc = "Search in Current Buffer" })
-map("n", "<leader>fgf", ":Telescope git_files<CR>", { desc = "Show Git Files" })
-map("n", "<leader>fgb", ":Telescope git_branches<CR>", { desc = "Show Git Branches" })
-map("n", "<leader>fgc", ":Telescope git_commits<CR>", { desc = "Show Git Commits" })
-map("n", "<leader>fgs", ":Telescope git_status<CR>", { desc = "Show Git Status" })
-map("n", "<leader>flr", ":Telescope lsp_references<CR>", { desc = "Show LSP References" })
-map("n", "<leader>fn", ":Telescope notify<CR>", { desc = "Show Notifications" })
-map("n", "<leader>ft", ":TodoTelescope<CR>", { desc = "Show TODOs" })
-map("n", "<leader>fi", ":Telescope import<CR>", { desc = "Show Imports" })
+map("n", "<leader>th", ":Telescope colorscheme<CR>", "Choose Colorscheme")
+map("n", "<leader>ff", ":Telescope find_files<CR>", "Find Files")
+map("n", "<leader>fw", ":Telescope live_grep<CR>", "Live Grep")
+map("n", "<leader>fo", ":Telescope oldfiles<CR>", "Show Recent Files")
+map("n", "<leader>fb", ":Telescope buffers<CR>", "Show Buffers")
+map("n", "<leader>fk", ":Telescope keymaps<CR>", "Show Keymaps")
+map("n", "<leader>fcb", ":Telescope current_buffer_fuzzy_find<CR>", "Search in Current Buffer")
+map("n", "<leader>fgf", ":Telescope git_files<CR>", "Show Git Files")
+map("n", "<leader>fgb", ":Telescope git_branches<CR>", "Show Git Branches")
+map("n", "<leader>fgc", ":Telescope git_commits<CR>", "Show Git Commits")
+map("n", "<leader>fgs", ":Telescope git_status<CR>", "Show Git Status")
+map("n", "<leader>flr", ":Telescope lsp_references<CR>", "Show LSP References")
+map("n", "<leader>fn", ":Telescope notify<CR>", "Show Notifications")
+map("n", "<leader>ft", ":TodoTelescope<CR>", "Show TODOs")
+map("n", "<leader>fi", ":Telescope import<CR>", "Show Imports")
 
 -- LSP
-map("n", "<leader>ld", vim.diagnostic.setloclist, { desc = "LSP List Diagnostics" })
-map("n", "[d", vim.diagnostic.goto_prev, { desc = "LSP Previous Diagnostic" })
-map("n", "]d", vim.diagnostic.goto_next, { desc = "LSP Next Diagnostic" })
-map("n", "<leader>lca", vim.lsp.buf.code_action, { desc = "LSP Code Actions" })
-map("n", "<leader>lr", vim.lsp.buf.rename, { desc = "LSP Rename" })
+map("n", "<leader>ld", vim.diagnostic.setloclist, "LSP List Diagnostics")
+map("n", "[d", vim.diagnostic.goto_prev, "LSP Previous Diagnostic")
+map("n", "]d", vim.diagnostic.goto_next, "LSP Next Diagnostic")
+map("n", "<leader>lca", vim.lsp.buf.code_action, "LSP Code Actions")
+map("n", "<leader>lr", vim.lsp.buf.rename, "LSP Rename")
 
 -- Conform
 map("n", "<leader>fm", function()
   require("conform").format({ async = true })
-end, { desc = "Format Buffer" })
+end, "Format Buffer")
 
 -- Outline
-map("n", "<leader>ps", ":Outline<CR>", { desc = "Toggle Outline" })
+map("n", "<leader>ps", ":Outline<CR>", "Toggle Outline")
 
 -- Markdown
-map("n", "<leader>mp", ":MarkdownPreviewToggle<CR>", { desc = "Toggle Markdown Preview" })
-map("n", "<leader>mr", ":Markview toggle<CR>", { desc = "Toggle Markdown Rendering" })
+map("n", "<leader>mp", ":MarkdownPreviewToggle<CR>", "Toggle Markdown Preview")
+map("n", "<leader>mr", ":Markview toggle<CR>", "Toggle Markdown Rendering")
 
 -- Gitsigns
-map("n", "<leader>ghp", ":Gitsigns preview_hunk<CR>", { desc = "Preview Hunk" })
-map("n", "<leader>glb", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle Current Line Git Blame" })
+map("n", "<leader>ghp", ":Gitsigns preview_hunk<CR>", "Preview Hunk")
+map("n", "<leader>glb", ":Gitsigns toggle_current_line_blame<CR>", "Toggle Current Line Git Blame")
 map("n", "<leader>gb", function()
   require("gitsigns").blame_line({ full = true })
-end, { desc = "Show Git Blame" })
+end, "Show Git Blame")
 
 -- Fugitive
-map("n", "<leader>gac", ":Gwrite<CR>", { desc = "Git Add Current File" })
+map("n", "<leader>gac", ":Gwrite<CR>", "Git Add Current File")
 
 -- Diffview
-map("n", "<leader>gdo", ":DiffviewOpen<CR>", { desc = "Open Diffview" })
-map("n", "<leader>gdc", ":DiffviewClose<CR>", { desc = "Close Diffview" })
+map("n", "<leader>gdo", ":DiffviewOpen<CR>", "Open Diffview")
+map("n", "<leader>gdc", ":DiffviewClose<CR>", "Close Diffview")
 
 -- AICommit
-map("n", "<leader>ac", ":AICommit<CR>", { desc = "Generate Commit Message for Staged Files" })
+map("n", "<leader>ac", ":AICommit<CR>", "Generate Commit Message for Staged Files")
 
 -- mini.diff
 map("n", "<leader>gmd", function()
   require("mini.diff").toggle_overlay()
-end, { desc = "Toggle mini.diff Overlay" })
+end, "Toggle mini.diff Overlay")
 
 -- Compiler
-map("n", "<leader>co", ":CompilerOpen<CR>", { desc = "Open Compiler Menu" })
-map("n", "<leader>cr", ":CompilerRedo<CR>", { desc = "Re-run Compiler Last Selected Option" })
-map("n", "<leader>ct", ":CompilerToggleResults<CR>", { desc = "Toggle Compiler Results" })
+map("n", "<leader>co", ":CompilerOpen<CR>", "Open Compiler Menu")
+map("n", "<leader>cr", ":CompilerRedo<CR>", "Re-run Compiler Last Selected Option")
+map("n", "<leader>ct", ":CompilerToggleResults<CR>", "Toggle Compiler Results")
