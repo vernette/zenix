3030613c3660d7a4c0d09b1185dd0ca52e6bd33d
Nikita Skryabin <nekit2271@gmail.com>
2025-04-03 18:11:11 +0300

    refactor(nvim/lsp): switch to blink.cmp for capabilities

--- nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -6,7 +6,7 @@ return {
     local lsp_settings = require("configs.lspconfig")
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+    capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 
     local signs = {
       Error = " ",
