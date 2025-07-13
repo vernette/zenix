4cdbefb5cfc6a20db1f50cac9a8e39f924d37d5b
Nikita Skryabin <nekit2271@gmail.com>
2025-07-13 21:12:34 +0300

    feat(nvim/blink): pin version

--- nvim/.config/nvim/lua/plugins/completions/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/completions/blink-cmp.lua
@@ -1,7 +1,7 @@
 return {
   "saghen/blink.cmp",
   dependencies = { "onsails/lspkind.nvim" },
-  version = "1.*",
+  version = "1.4.0",
   event = { "InsertEnter", "CmdlineEnter" },
   opts = require("configs.blink-cmp"),
 }
