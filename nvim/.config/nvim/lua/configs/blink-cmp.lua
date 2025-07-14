1ca29f20bb30ec63469cde4a83eca26147f4b1a0
Nikita Skryabin <nekit2271@gmail.com>
2025-07-14 17:53:23 +0300

    feat(nvim/blink): score_offset for completion providers

--- nvim/.config/nvim/lua/configs/blink-cmp.lua
+++ nvim/.config/nvim/lua/configs/blink-cmp.lua
@@ -1,10 +1,23 @@
 return {
+  appearance = {
+    nerd_font_variant = "normal",
+  },
+
   keymap = { preset = "super-tab" },
-  cmdline = { enabled = true },
+
+  fuzzy = {
+    implementation = "prefer_rust",
+  },
+
   completion = {
-    -- Enable auto brackets
     accept = { auto_brackets = { enabled = true } },
   },
+
+  cmdline = {
+    keymap = { preset = "inherit" },
+    completion = { menu = { auto_show = true } },
+  },
+
   sources = {
     default = {
       "lsp",
@@ -13,13 +26,30 @@ return {
       "path",
       "codeium",
     },
+
     providers = {
+      lsp = {
+        score_offset = 100,
+      },
+
+      snippets = {
+        score_offset = 80,
+      },
+
+      buffer = {
+        score_offset = 60,
+      },
+
       codeium = {
         name = "Codeium",
         module = "codeium.blink",
         async = true,
+        score_offset = 40,
+      },
+
+      path = {
+        score_offset = 20,
       },
     },
   },
-  fuzzy = { implementation = "prefer_rust" },
 }
