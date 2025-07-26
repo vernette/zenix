deacc03bf63a3580c603262d4006c8b753b90309
Nikita Skryabin <nekit2271@gmail.com>
2025-07-26 18:25:41 +0300

    feat(nvim/colorizer): add advanced colorizer options

--- nvim/.config/nvim/lua/plugins/ui/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/ui/colorizer.lua
@@ -1,5 +1,17 @@
 return {
   "catgoose/nvim-colorizer.lua",
   ft = { "html", "css" },
-  opts = {},
+  opts = {
+    filetypes = { "html", "css" },
+    user_default_options = {
+      names = false,
+      RGB = true,
+      RGBA = true,
+      RRGGBB = true,
+      RRGGBBAA = true,
+      AARRGGBB = true,
+      rgb_fn = true,
+      hsl_fn = true,
+    },
+  },
 }
