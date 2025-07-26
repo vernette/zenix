cff963a03eb4ea64359a4c230c4068a525b0153c
Nikita Skryabin <nekit2271@gmail.com>
2025-07-26 14:37:44 +0300

    feat(nvim/lint): add dynamic pyproject.toml configuration for ruff

--- nvim/.config/nvim/lua/configs/nvim-lint.lua
+++ nvim/.config/nvim/lua/configs/nvim-lint.lua
@@ -1,20 +1,50 @@
 local lint = require("lint")
 
+local function get_pyproject_path()
+  local current_file = vim.api.nvim_buf_get_name(0)
+  local current_dir = vim.fn.fnamemodify(current_file, ":p:h")
+
+  local found = vim.fs.find("pyproject.toml", {
+    path = current_dir,
+    upward = true,
+    type = "file",
+  })
+  return found[1] -- nil or full path
+end
+
+local ruff_args
+local pyproject_path = get_pyproject_path()
+
+if pyproject_path then
+  ruff_args = {
+    "check",
+    "--output-format",
+    "json",
+    "--config",
+    pyproject_path,
+    "--respect-gitignore",
+    "--preview",
+    "-",
+  }
+else
+  ruff_args = {
+    "check",
+    "--output-format",
+    "json",
+    "--select",
+    "E,F,UP,N,I,ASYNC,S,PTH,B,W,Q,C,R,G,PT",
+    "--ignore",
+    "Q000",
+    "--line-length",
+    "79",
+    "--respect-gitignore",
+    "--preview",
+    "-",
+  }
+end
+
 local ruff = lint.linters.ruff
-ruff.args = {
-  "check",
-  "--output-format",
-  "json",
-  "--select",
-  "E,F,UP,N,I,ASYNC,S,PTH,B,W,Q,C,R,G,PT",
-  "--ignore",
-  "Q000",
-  "--line-length",
-  "79",
-  "--respect-gitignore",
-  "--preview",
-  "-",
-}
+ruff.args = ruff_args
 
 return {
   linters_by_ft = {
