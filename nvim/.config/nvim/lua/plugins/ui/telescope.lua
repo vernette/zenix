c079af40299e44220a0247f286aa8cb575b4eb2a
Nikita Skryabin <nekit2271@gmail.com>
2025-07-26 18:39:26 +0300

    feat(nvim/telescope): add telescope-frecency plugin

--- nvim/.config/nvim/lua/plugins/ui/telescope.lua
+++ nvim/.config/nvim/lua/plugins/ui/telescope.lua
@@ -5,6 +5,7 @@ return {
     "piersolenski/telescope-import.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "debugloop/telescope-undo.nvim",
+    "nvim-telescope/telescope-frecency.nvim",
   },
   cmd = "Telescope",
   config = function()
@@ -13,5 +14,6 @@ return {
     telescope.load_extension("import")
     telescope.load_extension("fzf")
     telescope.load_extension("undo")
+    telescope.load_extension("frecency")
   end,
 }
