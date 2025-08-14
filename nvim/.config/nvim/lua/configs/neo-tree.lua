8a6b42ce79f1e0194cfcf744ba11838a2bf4b3da
Nikita Skryabin <nekit2271@gmail.com>
2025-08-14 11:39:11 +0300

    feat(nvim/neo-tree): add natural sort

--- nvim/.config/nvim/lua/configs/neo-tree.lua
+++ nvim/.config/nvim/lua/configs/neo-tree.lua
@@ -1,3 +1,9 @@
+local function pad_numbers(str)
+  return string.gsub(str, "%d+", function(n)
+    return string.format("%010d", tonumber(n))
+  end)
+end
+
 return {
   close_if_last_window = true,
 
@@ -21,6 +27,24 @@ return {
       hide_gitignored = true,
     },
   },
+
+  sort_function = function(a, b)
+    if not a or not b then
+      return false
+    end
+
+    local a_name = a.name or (a.path and vim.fn.fnamemodify(a.path, ":t")) or ""
+    local b_name = b.name or (b.path and vim.fn.fnamemodify(b.path, ":t")) or ""
+
+    local a_type = a.type or "file"
+    local b_type = b.type or "file"
+
+    if a_type ~= b_type then
+      return a_type == "directory"
+    end
+
+    return pad_numbers(a_name:lower()) < pad_numbers(b_name:lower())
+  end,
 
   source_selector = {
     winbar = true,
